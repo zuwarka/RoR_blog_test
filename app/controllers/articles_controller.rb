50023660b207c2f13ab85b206e7ed98f3d4d9c88
zuwarka <76390605+zuwarka@users.noreply.github.com>
2022-09-18 17:43:19 +0300

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[edit update show destroy]
   around_action :write_to_log
+  before_action :authenticate_user!
 
   def new
     @article = Article.new
